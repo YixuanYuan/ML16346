9fab5e9f4e982caddab50fc2f31de928887b5f7e
0v1d <52611302+MrMachine0@users.noreply.github.com>
2021-04-04 17:31:25 -0400

    Update ML.py

--- ML.py
+++ ML.py
@@ -8,25 +8,30 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 x_values = [0, 1, 2, 3, 4, 5, 6, 7]
-x_train = np.array(x_values, dtype=np.float32)
-x_train = x_train.reshape(-1, 1)
-x_np = torch.from_numpy(x_train)
+#x_train = np.array(x_values, dtype=np.float32)
+#x_train = x_train.reshape(-1, 1)
+#x_np = torch.from_numpy(x_train)
 
 y_values = [53807, 55217, 55209, 55415, 63100, 63206, 63761, 65766]
-y_train = np.array(y_values, dtype=np.float32)
-y_train = y_train.reshape(-1, 1)
+#y_train = np.array(y_values, dtype=np.float32)
+#y_train = y_train.reshape(-1, 1)
 
 slope, intercept, r, p, std_err = stats.linregress(x_values, y_values)
+
+print(stats.linregress(x_values, y_values))
 
 def func(x):
     return slope * x + intercept
 model = list(map(func, x_values))
 
-area = np.pi*3
-colors = (0,0,0)
+#area = np.pi*3
+#colors = (0,0,0)
 
-plt.scatter(x_values, y_values, s=area, c=colors, alpha=1)
+print(func(8))
+
+plt.scatter(x_values, y_values)
 plt.plot(x_values, model)
 plt.show()
+ 
 
 # test by Shawn
