9aca19e1a7b1564cbaab46334402737af1e61559
YixuanYuan <Yixuan.Yuan@thewalkerschool.org>
2021-04-04 09:20:47 -0400

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,13 +1,13 @@
 import torch
-<<<<<<< Updated upstream
 from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor, Lambda, Compose
 import matplotlib.pyplot as plt
-=======
-x = torch.rand(5, 3)
+
+
+x = torch.rand(5, 3)    
 print(x)
 print(torch.cuda.is_available())
->>>>>>> Stashed changes
+
 
