31cf8bf94017451ee7e071129a125dec98b4efcc
0v1d <52611302+MrMachine0@users.noreply.github.com>
2021-04-04 11:21:30 -0400

    Changing Year value to Years since 1995 for x values

--- ML.py
+++ ML.py
@@ -6,7 +6,7 @@ from torchvision.transforms import ToTensor, Lambda, Compose
 import numpy as np
 import matplotlib.pyplot as plt
 
-x_values = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002]
+x_values = [0, 1, 2, 3, 4, 5, 6, 7]
 x_train = np.array(x_values, dtype=np.float32)
 x_train = x_train.reshape(-1, 1)
 
